f9d08fbb2dd8ef74b7320899a05e5bca20763530
Jorge Ramirez-Ortiz <jorge@foundries.io>
2023-12-01 16:31:43 +0100

    mmc: rpmb: fixes pause retune on all RPMB partitions.
    commit e7794c14fd73e5eb4a3e0ecaa5334d5a17377c50 upstream.
    
    When RPMB was converted to a character device, it added support for
    multiple RPMB partitions (Commit 97548575bef3 ("mmc: block: Convert RPMB to
    a character device").
    
    One of the changes in this commit was transforming the variable target_part
    defined in __mmc_blk_ioctl_cmd into a bitmask. This inadvertently regressed
    the validation check done in mmc_blk_part_switch_pre() and
    mmc_blk_part_switch_post(), so let's fix it.
    
    Fixes: 97548575bef3 ("mmc: block: Convert RPMB to a character device")
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Cc: <stable@vger.kernel.org>
    Link: https://lore.kernel.org/r/20231201153143.1449753-1-jorge@foundries.io
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/core/block.c
+++ drivers/mmc/core/block.c
@@ -1290,10 +1290,11 @@ static const struct block_device_operations mmc_bdops = {
 static int mmc_blk_part_switch_pre(struct mmc_card *card,
 				   unsigned int part_type)
 {
+	const unsigned int mask = EXT_CSD_PART_CONFIG_ACC_RPMB;
 	int ret = 0;
 
 #if defined(CONFIG_MTK_EMMC_CQ_SUPPORT) || defined(CONFIG_MTK_EMMC_HW_CQ)
-	/* disabe cmdq
+    /* disabe cmdq
 	 * if partition does not support cmdq
 	 */
 	if (mmc_card_cmdq(card)
@@ -1302,28 +1303,29 @@ static int mmc_blk_part_switch_pre(struct mmc_card *card,
 		if (ret)
 			return ret;
 	}
-	if (part_type == EXT_CSD_PART_CONFIG_ACC_RPMB)
+	if ((part_type & mask) == mask)
 		mmc_retune_pause(card->host);
 #else
-	if (part_type == EXT_CSD_PART_CONFIG_ACC_RPMB) {
-		if (card->ext_csd.cmdq_en) {
-			ret = mmc_cmdq_disable(card);
-			if (ret)
-				return ret;
-		}
-		mmc_retune_pause(card->host);
-	}
+    if ((part_type & mask) == mask) {
+        if (card->ext_csd.cmdq_en) {
+            ret = mmc_cmdq_disable(card);
+            if (ret)
+                return ret;
+        }
+        mmc_retune_pause(card->host);
+    }
 #endif
-	return ret;
+    return ret;
 }
 
 static int mmc_blk_part_switch_post(struct mmc_card *card,
 				    unsigned int part_type)
 {
+	const unsigned int mask = EXT_CSD_PART_CONFIG_ACC_RPMB;
 	int ret = 0;
 
 #if defined(CONFIG_MTK_EMMC_CQ_SUPPORT) || defined(CONFIG_MTK_EMMC_HW_CQ)
-	if (part_type == EXT_CSD_PART_CONFIG_ACC_RPMB)
+    if ((part_type & mask) == mask)
 		mmc_retune_unpause(card->host);
 
 	/* enable cmdq
@@ -1336,14 +1338,14 @@ static int mmc_blk_part_switch_post(struct mmc_card *card,
 	}
 #else
 
-	if (part_type == EXT_CSD_PART_CONFIG_ACC_RPMB) {
-		mmc_retune_unpause(card->host);
-		if (card->reenable_cmdq && !card->ext_csd.cmdq_en)
-			ret = mmc_cmdq_enable(card);
-	}
+    if ((part_type & mask) == mask) {
+        mmc_retune_unpause(card->host);
+        if (card->reenable_cmdq && !card->ext_csd.cmdq_en)
+            ret = mmc_cmdq_enable(card);
+    }
 #endif
 
-	return ret;
+    return ret;
 }
 
 static inline int mmc_blk_part_switch(struct mmc_card *card,
@@ -5111,4 +5113,3 @@ module_exit(mmc_blk_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Multimedia Card (MMC) block device driver");
-
